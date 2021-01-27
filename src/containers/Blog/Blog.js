ada232ded0cc7a54217db9ce87536239048cc714
Alex <cigan@ua.fm>
2021-01-27 19:33:56 +0200

    Add a method to show  more info about Post in FullPost using axios.get

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -10,6 +10,7 @@ class Blog extends Component {
 
     state = {
         posts: [],
+        selectedPostId: null,
     }
 
     componentDidMount() {
@@ -24,12 +25,23 @@ class Blog extends Component {
                 })
 
                 this.setState({posts: updatePosts});
+                // console.log(this.state.posts);
             });
     }
+
+    postSelectedHandler = (id) => {
+        this.setState({selectedPostId: id});
+    }
+
     render() {
 
         const posts = this.state.posts.map(post => {
-            return <Post key={post.id} title={post.title} author={post.author} />
+            return <Post
+                key={post.id}
+                title={post.title}
+                author={post.author}
+                clicked={() => this.postSelectedHandler(post.id)}
+            />
         });
 
         return (
@@ -38,7 +50,7 @@ class Blog extends Component {
                     {posts}
                 </section>
                 <section>
-                    <FullPost />
+                    <FullPost id={this.state.selectedPostId} />
                 </section>
                 <section>
                     <NewPost />
