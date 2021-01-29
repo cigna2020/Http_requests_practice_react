1be833c147ee808dccd8476fa6afa35868c89cbc
Alex <cigan@ua.fm>
2021-01-29 17:06:21 +0200

    Install react-router-dom, cover <Blog> component with <BrowserRouter> and use <Route /> to add pathes

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,75 +1,28 @@
 import React, {Component} from 'react';
-import axios from 'axios';
+import {Route} from 'react-router-dom';
 
-import Post from '../../components/Post/Post';
-import FullPost from '../../components/FullPost/FullPost';
-import NewPost from '../../components/NewPost/NewPost';
+
+import Posts from '../Posts/Posts';
 import './Blog.css';
 
 class Blog extends Component {
 
-    state = {
-        posts: [],
-        selectedPostId: null,
-        errors: false,
-        errorMessage: '',
-    }
-
-    componentDidMount() {
-        axios.get('/posts')
-            .then(response => {
-                const posts = response.data.slice(0, 4);
-                const updatePosts = posts.map(post => {
-                    return {
-                        ...post,
-                        author: 'Alex'
-                    }
-                })
-
-                this.setState({posts: updatePosts});
-                // console.log(this.state.posts);
-            })
-            .catch(error => {
-                // console.log("ERROR:" + error)
-                const newError = error.toString()
-                this.setState({errors: true, errorMessage: newError});
-            });
-    }
-
-    postSelectedHandler = (id) => {
-        this.setState({selectedPostId: id});
-    }
-
     render() {
-        let posts = this.state.posts.map(post => {
-            return <Post
-                key={post.id}
-                title={post.title}
-                author={post.author}
-                clicked={() => this.postSelectedHandler(post.id)}
-            />
-        });
-        if (this.state.errors) {
-            posts =
-                <div>
-                    <p>Something wrong! </p>
-                    <p>{this.state.errorMessage}</p>
-                </div>
-        };
-
-
-
         return (
-            <div>
-                <section className="Posts">
-                    {posts}
-                </section>
-                <section>
-                    <FullPost id={this.state.selectedPostId} />
-                </section>
-                <section>
-                    <NewPost />
-                </section>
+            <div className="Blog">
+                <header >
+                    <nav>
+                        <ul>
+                            <li><a href="/">Home</a></li>
+                            <li><a href="/new-post">New Post</a></li>
+                        </ul>
+                    </nav>
+                </header>
+                {/* <Route path="/"  render={() => <h1>HOME</h1>} /> */}
+                <Route path="/new-post" render={() => <h1>NEW POST</h1>} />
+                {/* <Route path="/" exact render={() => <Posts />} /> */}
+                <Route path="/" exact component={Posts} />
+
             </div>
         );
     }
---
+++ src/containers/Posts/Posts.js
@@ -0,0 +1,62 @@
+import React, {Component} from 'react';
+import './Posts.css';
+import axios from 'axios';
+
+import Post from '../../components/Post/Post';
+
+class Posts extends Component {
+    state = {
+        posts: [],
+    }
+
+    componentDidMount() {
+        axios.get('/posts')
+            .then(response => {
+                const posts = response.data.slice(0, 4);
+                const updatePosts = posts.map(post => {
+                    return {
+                        ...post,
+                        author: 'Alex'
+                    }
+                })
+
+                this.setState({posts: updatePosts});
+                // console.log(this.state.posts);
+            })
+            .catch(error => {
+                console.log("ERROR:" + error)
+                // const newError = error.toString()
+                // this.setState({errors: true, errorMessage: newError});
+            });
+    }
+
+    postSelectedHandler = (id) => {
+        this.setState({selectedPostId: id});
+    }
+
+    render() {
+        let posts = this.state.posts.map(post => {
+            return <Post
+                key={post.id}
+                title={post.title}
+                author={post.author}
+                clicked={() => this.postSelectedHandler(post.id)}
+            />
+        });
+        if (this.state.errors) {
+            posts =
+                <div>
+                    <p>Something wrong! </p>
+                    <p>{this.state.errorMessage}</p>
+                </div>
+        };
+
+        return (
+            <section className="Posts">
+                {posts}
+            </section>
+        )
+    }
+};
+
+export default Posts;
