b687b0c491e03ad1d2d564550888eac06fcc8179
Alex <cigan@ua.fm>
2021-01-30 12:03:53 +0200

    Change <Link/> on <NavLink/> to add a class "active" automatically

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {Route, Link} from 'react-router-dom';
+import {Route, NavLink} from 'react-router-dom';
 
 
 import Posts from '../Posts/Posts';
@@ -16,12 +16,18 @@ class Blog extends Component {
                         <ul>
                             {/* <li><a href="/">Home</a></li>
                             <li><a href="/new-post">New Post</a></li> */}
-                            <Link to="/" className='link'>Home</Link>
-                            <Link className='link' to={{
-                                pathname: '/new-post',
-                                hash: '#submit',
-                                search: '?quick-submit=true'
-                            }}>New Post</Link>
+                            <NavLink to="/" exact className='link'>Home</NavLink>
+                            <NavLink className='link'
+                                activeClassName="my-active"  // щоб змінити дефолтне значення 'active'
+                                activeStyle={{              // стилі, що будуть додані після кліка. Щоб не використовувати css files
+                                    color: 'red',
+                                    textDecoration: 'underline'
+                                }}
+                                to={{
+                                    pathname: '/new-post',
+                                    hash: '#submit',
+                                    search: '?quick-submit=true'
+                                }}>New Post</NavLink>
                         </ul>
                     </nav>
                 </header>
