f6faf4aeb582f35b73783270d8c6a297092614b3
Alex <cigan@ua.fm>
2021-01-30 14:47:23 +0200

    Add <Switch> and wrap few Routes to load only one <Route> (component) for one path
    Be careful, the order of the Routes is more important than without <Switch>

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {Route, NavLink} from 'react-router-dom';
+import {Route, NavLink, Switch} from 'react-router-dom';
 
 
 import Posts from '../Posts/Posts';
@@ -35,9 +35,10 @@ class Blog extends Component {
                 {/* <Route path="/"  render={() => <h1>HOME</h1>} /> */}
                 {/* <Route path="/" exact render={() => <Posts />} /> */}
                 <Route path="/" exact component={Posts} />
-                <Route path="/:id" exact component={FullPost} />
-                <Route path="/new-post" exact component={NewPost} />
-
+                <Switch>
+                    <Route path="/new-post" exact component={NewPost} />
+                    <Route path="/:id" exact component={FullPost} />
+                </Switch>
             </div>
         );
     }
